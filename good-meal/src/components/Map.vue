5b11972047320f09d26685a1e54dc061f75c8f73
Gregory <huyghegregory@yahoo.fr>
2019-03-18 15:51:03 +0100

    projet vue CLI créé, début travail intégration carte Google Maps

---
+++ good-meal/src/components/Map.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="map">
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Map',
+    data: function () {
+      
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    
+
+</style>
